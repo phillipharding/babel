ac258f68e5fce29989b75aef898ebe5ef3577219
Phillip Allan-Harding <phil.harding@platinumdogs.co.uk>
2014-11-28 14:43:52 +0000

    Buzz v1-1 changeset 1

--- SharePoint-CSOM/buzz365/resources/js/site.js
+++ SharePoint-CSOM/buzz365/resources/js/site.js
@@ -2,6 +2,8 @@
 "use strict";
 
 	$(function() {
+		JSRequest.EnsureSetup();
+		/*
 		$("ul#tabs li").click(function(e) {
 			if (!$(this).hasClass("active")) {
 				var tabNum = $(this).index();
@@ -12,14 +14,63 @@
 				$("ul#tab li:nth-child(" + nthChild + ")").addClass("active");
 			}
 		});
+		*/
 
+		/* init SIDR for mobile search */
+//		$(window).resize(function() {
+//			$.sidr('close', 'sidr-search');
+//		});
+		
+//		$('#mobile-search').sidr({
+//			name: 'sidr-search',
+//			source: '#mobile-header-search-box',
+//			side: 'right'
+//		});
+//		var searchQuery = JSRequest.QueryString["k"] || "";
+//		$('#sidr-id-search-input-box').val(searchQuery);
+/*		$('#sidr-id-search-input-box').on('keypress', function(event) {
+			EnsureScriptFunc('Search.ClientControls.js', 'Srch.U', function() {
+				if (Srch.U.isEnterKey(String.fromCharCode(event.keyCode))) {
+					var searchTerm = $('#sidr-id-search-input-box').val();
+					searchTerm += String.format("site:", encodeURIComponent(_spPageContextInfo.webAbsoluteUrl));
+
+					$find('ctl00_searchInputBox_csr').search(searchTerm);
+					$.sidr('close', 'sidr-search');
+					return Srch.U.cancelEvent(event);
+				}
+			});
+		});*/
+		$('#mobile-search').click(function(e) {
+			e.preventDefault();
+			$(this).toggleClass('active');
+			$('#mobile-header-search-box').toggleClass('active');
+			/*
+			EnsureScriptFunc('Search.ClientControls.js', 'Srch.U', function() {
+				var searchTerm = $('#sidr-id-search-input-box').val();
+				searchTerm += String.format("site:", encodeURIComponent(_spPageContextInfo.webAbsoluteUrl));
+				$find('ctl00_searchInputBox_csr').search(searchTerm);
+				$.sidr('close', 'sidr-search');
+			});
+			*/
+		});
+		/**/
+
+		/* init SIDR for the megamenu */
 		$(window).resize(function() {
 			$.sidr('close', 'sidr-existing-content');
 		});
 		
 		$('#mobile-nav').sidr({
 			name: 'sidr-existing-content',
-			source: '#main-menu'
+			source: '#main-menu',
+			onOpen: function() {
+				$('#mobile-nav').addClass('active');
+				$('#mobile-search').removeClass('active');
+				$('#mobile-header-search-box').removeClass('active');
+			},
+			onClose: function() {
+				$('#mobile-nav').removeClass('active');
+			}
 		});
 		
 		/* Toggle Off Canvas Menu */
@@ -33,6 +84,8 @@
 				ullist.show('5000');
 			}
 		});
+		/**/
+
 	});
 
 })(jQuery);
