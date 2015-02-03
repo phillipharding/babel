614a2e80cf547c6fe6078f83f1f973e5e1cdd4e1
Phil Harding <phil.harding@platinumdogs.co.uk>
2015-02-03 12:22:27 +0000

    rbcom provision masterpages to site collections

--- SharePoint-CSOM/Modules/load-spo-modules.ps1
+++ SharePoint-CSOM/Modules/load-spo-modules.ps1
@@ -8,12 +8,13 @@ Import-Module "$modulesPath\Load-CSOM.psm1"
 Add-InternalDlls -assemblyPath $assemblyPath -excludeDlls "*.HttpCommands.dll"
 #Add-InternalDlls -assemblyPath $assemblyPath 
 
+Write-Host "Import Modules: " -ForegroundColor White -NoNewLine
 $modules = @("ClientConnection.psm1","Columns.psm1","ContentTypes.psm1","Files.psm1","Features.psm1","Items.psm1","Lists.psm1","Permissions.psm1","PropertyBag.psm1","Publishing.psm1","Sites.psm1","Taxonomy.psm1","CustomActions.psm1","Webs.psm1","SearchCenter.psm1")
 $modules | % {
-   Write-Host "Import Module:" -ForegroundColor White -NoNewLine
-   Write-Host " [$_]" -ForegroundColor Yellow
+   Write-Host "$($_ -replace `".psm1$`",`"`") " -ForegroundColor Yellow -NoNewLine
    Import-Module "$modulesPath\$_" -DisableNameChecking
 }
+Write-Host 
 <#
    Excluding the ManagedProperties extensions since this is probably 
    better managed via the Search Schema Import/Export functionality
